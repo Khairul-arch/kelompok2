6c8c7dd5cb92e81e6ed38bec6ede4b77c0105f1a
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-08 17:10:20 +0700

    status

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
+    char bukuTersedia[][10] = {"BK001", "BK002", "BK003", "BK004"};
+    int jumlah = 4;
+    char kode[10];
+    int ditemukan = 0;
 
+void statusBuku() {
+    printf("Masukkan kode buku: ");
+    scanf("%s", kode);
 
+    // Cek apakah kode ada di daftar
+    for (int i = 0; i < jumlah; i++) {
+        if (strcmp(kode, bukuTersedia[i]) == 0) {
+            ditemukan = 1;
+            break;
+        }
+    }
 
-
-
+    // Output status
+    if (ditemukan)
+        printf("Status buku: tersedia\n");
+    else
+        printf("Status buku: dipinjam\n");
+}
 
 int main() {
     int opsi, SubOpsi1, SubOpsi2, SubOpsi3;
